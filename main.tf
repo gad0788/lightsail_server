0dd7f1d70d4e8094d800a129df0edeb4cb2d6a09
gad <nday.gady@gmail.com>
2024-09-24 19:30:09 -0700

    lightsail code

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_lightsail_instance" "server"{
+name = "server"
+blueprint_id = "centos_stream_9"
+availability_zone = "us-east-1a"
+bundle_id = "small_1_0"
+tags = {
+  Name = "server"
+  Environment = "test"
+}
+}
+
+
